c3ac8b5990007713ff263f2e5c977e0036f9a4bf
blake-lim <blake198492@kakao.com>
2024-04-19 11:15:24 +0900

    ENH : MVVM 아키텍쳐 폴더구조 생성 및 디자인 시스템 추가

---
+++ lib/constans/styles.dart
@@ -0,0 +1,32 @@
+/* 색상 값, 정의*/
+import 'dart:ui';
+
+const Color value000 = Color(0xFF7256E9);
+const Color value001 = Color(0xFFBEB6FA);
+const Color value100 = Color(0xFFDCFB7E);
+const Color value101 = Color(0x0ffeffcd);
+const Color value200 = Color(0xFFA47A0E);
+const Color value201 = Color(0xFFFFD772);
+
+const Color surface000 = Color(0xFFF4EFFF);
+const Color surface001 = Color(0xFFF9F6FF);
+const Color surface002 = Color(0xFFE3E7F7);
+const Color surface100 = Color(0xFFAEBEAA);
+const Color surface101 = Color(0xFFE6EDE5);
+const Color surface200 = Color(0xFFFAF8F3);
+const Color surface300 = Color(0xFFFFF0C9);
+
+const Color gray999 = Color(0xFF000000);
+const Color gray500 = Color(0xFFB8B8B8);
+const Color gray400 = Color(0xFF8C8993);
+const Color gray300 = Color(0xFFA9A6B5);
+const Color gray200 = Color(0xFFE9E9E9);
+const Color gray100 = Color(0xFFF8F8F8);
+const Color gray000 = Color(0xFFFFFFFF);
+
+const Color system000 = Color(0xFFFF0000);
+const Color system100 = Color(0xFF0A84FF);
+
+const dim000 = Color(0x66000000);
+const dim100 = Color(0x99000000);
+const dim900 = Color(0x80FFFFFF);
