cb2c0c08767fec2b0cd9c394889ef5942fa42679
blake-lim <blake198492@kakao.com>
2024-04-20 13:49:17 +0900

    ENH : 글로벌 컨펌 버튼 추가

--- lib/viewmodels/universal_view_model.dart
+++ lib/viewmodels/universal_view_model.dart
@@ -6,6 +6,16 @@ import 'package:harmony_hub/services/resource_manager.dart';
 // 공통적으로 사용하는 뷰모델
 //------------------------------------------------------------------------------
 class UniversalViewModel extends GetxController {
+  var selectedInstrument = Rxn<String>();
+
+  void selectInstrument(String instrumentName) {
+    if (selectedInstrument.value == instrumentName) {
+      selectedInstrument.value = null;
+    } else {
+      selectedInstrument.value = instrumentName;
+    }
+  }
+
   List<Map<String, String>> getSongsByGenre(String genre) {
     return ResourceManager.songList
         .where((song) => song['genre'] == genre)
--- lib/views/select_instruments/components/instruments_tile.dart
+++ lib/views/select_instruments/components/instruments_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:harmony_hub/constans/styles.dart';
+import 'package:harmony_hub/viewmodels/universal_view_model.dart';
 
 //------------------------------------------------------------------------------
 // 악기 개별 아이템 위젯
@@ -16,29 +18,47 @@ class InstrumentTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(
-        title: Center(
-          child: Text(
-            name,
-            style: const TextStyle(
-              color: gray000,
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
+    final viewModel = Get.find<UniversalViewModel>();
+
+    return Obx(() => GestureDetector(
+          onTap: () => viewModel.selectInstrument(name),
+          child: GridTile(
+            footer: GridTileBar(
+              backgroundColor: Colors.black45,
+              title: Center(
+                child: Text(
+                  name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: viewModel.selectedInstrument.value == name
+                      ? Container(
+                          color: Colors.black.withOpacity(0.8),
+                          alignment: Alignment.center,
+                          child: const Icon(Icons.check,
+                              color: Colors.white, size: 48),
+                        )
+                      : Container(),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(width: 1.0, color: dim900),
-          image: DecorationImage(
-            image: AssetImage(imagePath),
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
