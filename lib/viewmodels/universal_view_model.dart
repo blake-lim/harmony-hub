fe1e8f3d8804980a71a56bc61dbe429de029f6f2
blake-lim <blake198492@kakao.com>
2024-04-20 12:05:30 +0900

    ENH : Universal ViewModel 추가

--- lib/viewmodels/universal_view_model.dart
+++ lib/viewmodels/universal_view_model.dart
@@ -1,3 +1,30 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class UniversalViewModel extends GetxController {}
+//------------------------------------------------------------------------------
+// 공통적으로 사용하는 뷰모델
+//------------------------------------------------------------------------------
+class UniversalViewModel extends GetxController {
+  final RxList<Map<String, String>> firstLineGenres = <Map<String, String>>[
+    {'name': 'Rock', 'image': 'assets/images/rock.jpg'},
+    {'name': 'Hip-Hop', 'image': 'assets/images/hiphop.jpg'},
+    {'name': 'Pop', 'image': 'assets/images/pop.jpg'},
+    {'name': 'Electronic', 'image': 'assets/images/electronic.jpg'},
+  ].obs;
+
+  final RxList<Map<String, String>> secondLineGenres = <Map<String, String>>[
+    {'name': 'Folk', 'image': 'assets/images/folk.jpg'},
+    {'name': 'Blues', 'image': 'assets/images/blues.jpg'},
+    {'name': 'Jazz', 'image': 'assets/images/jazz.jpg'},
+    {'name': 'Classical', 'image': 'assets/images/classical.jpg'},
+  ].obs;
+
+  void loadImages() {
+    for (var genre in firstLineGenres) {
+      precacheImage(AssetImage(genre['image']!), Get.context!);
+    }
+    for (var genre in secondLineGenres) {
+      precacheImage(AssetImage(genre['image']!), Get.context!);
+    }
+  }
+}
